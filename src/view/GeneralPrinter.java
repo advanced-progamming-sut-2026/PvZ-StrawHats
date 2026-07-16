b9de4da2565b2af4dd68b5bd672e40bc17edca68
ERSA2020 <erfan86salehi@gmail.com>
2026-07-16 14:05:23 +0330

    fix GeneralPrinter

--- src/view/GeneralPrinter.java
+++ src/view/GeneralPrinter.java
@@ -1,5 +1,11 @@
 package view;
 
 public class GeneralPrinter {
+    public static void print(String message) {
+        System.out.println(message);
+    }
 
+    public static void printErr(String errorMessage) {
+        System.err.println("error :"+errorMessage);
+    }
 }
