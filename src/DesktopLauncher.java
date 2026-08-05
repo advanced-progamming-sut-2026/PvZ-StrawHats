5eebd93f504516abaea0a24c027fd2be5301b455
arvintalebi261 <arvintalebi261@gmail.com>
2026-08-05 14:39:46 -0700

    icon

--- src/DesktopLauncher.java
+++ src/DesktopLauncher.java
@@ -1,15 +1,23 @@
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-
 public class DesktopLauncher {
     public static void main(String[] args) {
-        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
         config.setTitle("Plants vs Zombies");
         config.setWindowedMode(800, 600);
         config.setResizable(false);
         config.setForegroundFPS(60);
 
+        config.setWindowIcon(
+                Files.FileType.Internal,
+                "images/ui/icon.png",
+                "images/ui/icon.png"
+        );
+
         new Lwjgl3Application(new Main(), config);
     }
+
+
 }
