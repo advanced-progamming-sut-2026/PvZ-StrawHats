078af9bf648892f6f40e6f1c9bed5b2acc10f3d6
ERSA2020 <erfan86salehi@gmail.com>
2026-08-08 10:31:21 +0330

    add click sound

--- src/service/resource_manager/AudioEnum.java
+++ src/service/resource_manager/AudioEnum.java
@@ -1,7 +1,7 @@
 package service.resource_manager;
 
 public enum AudioEnum {
-   SFX_CLICK(""),
+    SFX_CLICK("assets/audio/sfx/click 1.mp3"),
     MENU_MUSIC("assets/audio/music/loonboon_142032648.mp3");
 
     private final String filePath;
